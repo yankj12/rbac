b23fedfd1a3c3a4d34455d6fbced46f063468115
yankj12 <yankj12@163.com>
2017-11-14 14:14:48 +0800

    为表添加一些注释

--- db/mysql/tables-mysql.sql
+++ db/mysql/tables-mysql.sql
@@ -19,86 +19,92 @@ CREATE TABLE
 
 
 --用户表
+DROP TABLE IF EXISTS rbac_user;
 CREATE TABLE
     rbac_user
     (
-        userId VARCHAR(20) NOT NULL,
-        userName VARCHAR(60),
-        validStatus VARCHAR(2) DEFAULT '1',
-        insertTime DATETIME,
-        updateTime DATETIME,
+        userId VARCHAR(20) NOT NULL COMMENT '用户Id',
+        userName VARCHAR(60) COMMENT '用户名称',
+        validStatus VARCHAR(2) DEFAULT '1' COMMENT '有效状态',
+        insertTime DATETIME COMMENT '创建时间',
+        updateTime DATETIME COMMENT '创建时间',
         PRIMARY KEY (userId)
     )
-    ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
 
 --角色表
+DROP TABLE IF EXISTS rbac_role;
 CREATE TABLE
     rbac_role
     (
-        roleId VARCHAR(20) NOT NULL,
-        roleName VARCHAR(40),
-        validStatus VARCHAR(2) DEFAULT '1',
-        insertTime DATETIME,
-        updateTime DATETIME,
+        roleId VARCHAR(20) NOT NULL COMMENT '角色Id',
+        roleName VARCHAR(40) COMMENT '角色名称',
+        validStatus VARCHAR(2) DEFAULT '1' COMMENT '有效状态',
+        insertTime DATETIME COMMENT '创建时间',
+        updateTime DATETIME COMMENT '修改时间',
         PRIMARY KEY (roleId)
     )
-    ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
 
 --用户组表
+DROP TABLE IF EXISTS rbac_group;
 CREATE TABLE
     rbac_group
     (
-        groupId VARCHAR(20) NOT NULL,
-        groupName VARCHAR(60),
-        validStatus VARCHAR(2) DEFAULT '1',
-        insertTime DATETIME,
-        updateTime DATETIME,
+        groupId VARCHAR(20) NOT NULL COMMENT '组Id',
+        groupName VARCHAR(60) COMMENT '组名称',
+        validStatus VARCHAR(2) DEFAULT '1' COMMENT '有效状态',
+        insertTime DATETIME COMMENT '创建时间',
+        updateTime DATETIME COMMENT '修改时间',
         PRIMARY KEY (groupId)
     )
-    ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户组表';
 
 --组角色关系表
+DROP TABLE IF EXISTS rbac_group_role;
 CREATE TABLE
     rbac_group_role
     (
-        groupId VARCHAR(20) NOT NULL,
-        groupName VARCHAR(60),
-        roleId VARCHAR(20) NOT NULL,
-        roleName VARCHAR(60),
-        validStatus VARCHAR(2) DEFAULT '1',
-        insertTime DATETIME,
-        updateTime DATETIME,
+        groupId VARCHAR(20) NOT NULL COMMENT '组Id',
+        groupName VARCHAR(60) COMMENT '组名称',
+        roleId VARCHAR(20) NOT NULL COMMENT '角色Id',
+        roleName VARCHAR(60) COMMENT '角色名称',
+        validStatus VARCHAR(2) DEFAULT '1' COMMENT '有效状态',
+        insertTime DATETIME COMMENT '创建时间',
+        updateTime DATETIME COMMENT '修改时间',
         PRIMARY KEY (groupId, roleId)
     )
-    ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='组角色关系表';
 
 --用户组关系表
+DROP TABLE IF EXISTS rbac_user_group;
 CREATE TABLE
     rbac_user_group
     (
-        userId VARCHAR(20) NOT NULL,
-        userName VARCHAR(60),
-        groupId VARCHAR(20) NOT NULL,
-        groupName VARCHAR(60),
-        validStatus VARCHAR(2) DEFAULT '1',
-        insertTime DATETIME,
-        updateTime DATETIME,
+        userId VARCHAR(20) NOT NULL COMMENT '用户Id',
+        userName VARCHAR(60) COMMENT '用户名称',
+        groupId VARCHAR(20) NOT NULL COMMENT '组Id',
+        groupName VARCHAR(60) COMMENT '组名称',
+        validStatus VARCHAR(2) DEFAULT '1' COMMENT '有效状态',
+        insertTime DATETIME COMMENT '创建时间',
+        updateTime DATETIME COMMENT '修改时间',
         PRIMARY KEY (userId, groupId)
     )
-    ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户组关系表';
 
 --用户角色关系表
+DROP TABLE IF EXISTS rbac_user_role;
 CREATE TABLE
     rbac_user_role
     (
-        userId VARCHAR(20) NOT NULL,
-        userName VARCHAR(60),
-        roleId VARCHAR(20) NOT NULL,
-        roleName VARCHAR(60),
-        validStatus VARCHAR(2) DEFAULT '1',
-        insertTime DATETIME,
-        updateTime DATETIME,
+        userId VARCHAR(20) NOT NULL COMMENT '用户Id',
+        userName VARCHAR(60) COMMENT '用户名称',
+        roleId VARCHAR(20) NOT NULL COMMENT '角色Id',
+        roleName VARCHAR(60) COMMENT '角色名称',
+        validStatus VARCHAR(2) DEFAULT '1' COMMENT '有效状态',
+        insertTime DATETIME COMMENT '创建时间',
+        updateTime DATETIME COMMENT '修改时间',
         PRIMARY KEY (userId, roleId)
     )
-    ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户角色关系表';
 
